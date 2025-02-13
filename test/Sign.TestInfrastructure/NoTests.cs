60ce2bf0368cd0784394e20a46bdc428b9985d9f
Bradley Grainger <bgrainger@gmail.com>
2025-02-13 11:13:50 -0800

    Add Faithlife.Build and CI.

---
+++ test/Sign.TestInfrastructure/NoTests.cs
@@ -0,0 +1,16 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE.txt file in the project root for more information.
+
+using Xunit;
+
+namespace Sign.TestInfrastructure;
+
+public sealed class NoTests
+{
+    [Fact]
+    public void Pass()
+    {
+        Assert.True(true);
+    }
+}
